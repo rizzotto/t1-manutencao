522bd787163804471d9159d96d0d31a68bbb42ef
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2019-09-06 19:42:21 -0300

    3: Bruno, Eduardo e Rafael
    Ajustar conversão de/para modelo do Firebase e do app, e adicionar documentação no serviço de acesso ao Firebase.

--- src/Database/Firebase.js
+++ src/Database/Firebase.js
@@ -6,6 +6,10 @@ firebase.initializeApp(firebaseConfig);
 class FirebaseDatabase {
     _db = firebase.database();
 
+    // =====================================================
+    // ANAMNESE: ver arquivo `./AnamnesisRecord.js`
+    // =====================================================
+
     /**
      * Retorna a última anamnese do usuário
      * @param userId ID do usuário
@@ -38,18 +42,31 @@ class FirebaseDatabase {
      * @returns Object
      */
     saveAnamnesis = (userId, data) => {
+        // remover data de criação (será armazenada no path para a ficha)
         const date = data.creationDate;
         data.creationDate = null;
+
+        // converter data de nascimento de Date para string ISO (YYYY-MM-DD)
+        const birthDateDate = data.birthDate;
+        data.birthDate = birthDateDate.toISOString().split("T")[0];
 
         const timestamp = date.getTime();
         return this._db.ref(`${userId}/anamneses/${timestamp}`).set(data);
     }
 
+    /**
+     * Mapeia anamneses do formato do Firebase para o formato do app.
+     * Ver documentação de ambos os formatos em `./AnamnesisRecord.js`.
+     */
     _mapAnamneses = (value) => {
         return Object.keys(value)
             .map((timestamp) => {
                 const record = value[timestamp];
                 record.creationDate = new Date(parseInt(timestamp));
+
+                const birthDateISO = record.birthDate;
+                record.birthDate = new Date(birthDateISO);
+
                 return record;
             })
             .sort((a, b) => a.creationDate < b.creationDate);
