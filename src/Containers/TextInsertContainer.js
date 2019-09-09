e1464512e8635af66b71d8478463cac1ab86eaf6
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2019-09-09 18:48:00 -0300

    14: Bruno e Eduardo
    Documentação e refatoração.

--- src/Containers/TextInsertContainer.js
+++ src/Containers/TextInsertContainer.js
@@ -1,7 +1,7 @@
 /**
  * Container para tela com título, input de texto e botão para ação.
- * Utiliza os componentes:
- *
+ * Utiliza os componentes: TitleDescComponent, DefaultButtonComponent e ...
+ * Autores: Bruno Guerra e Eduardo Lessa
  */
 
 import React, {Component} from 'react';
@@ -11,17 +11,31 @@ import DefautlButtonComponent from "../Components/defaultButtonComponent";
 
 export default class TextInsertContainer extends Component{
 
+    /**
+     * Parâmetros
+     * @param title string com o título a ser exibido
+     * @param description string com a descrição doUso do TextInsertContainer título (opcional)
+     * @param titleDescViewStyle StyleSheet com os estilos do <View> do componente TitleDescComponent (opcional)
+     * @param titleDescStyle StyleSheet com os estilos do texto do componente TitleDescComponente (opcional)
+     * @param buttonViewStyle StyleSheet com os estilos do <View> do componente DefaultButtonComponent
+     * @param buttonText texto do botão do container
+     * 
+     * Uso do StyleSheet: StyleSheet.create({ ... });
+     * 
+     * Exemplo de uso: <TextInsertContainer title="Meus dados" buttonText="Enviar"/>
+     */
+
     render(){
         return(
             <View> 
                 <TitleDescComponent titleText={this.props.title} 
                     descriptionText={this.props.description} 
-                    styleTitle={styles.title}
-                    styleView={styles.titleView}
+                    styleTitle={[styles.title, this.props.titleDescStyle]} 
+                    styleView={[styles.titleView, this.props.titleDescViewStyle]}
                 />
                 <DefautlButtonComponent 
                     text={this.props.buttonText}
-                    viewStyle={styles.buttonView}
+                    viewStyle={[styles.buttonView, this.props.buttonViewStyle]}
                 />
             </View>
         )
