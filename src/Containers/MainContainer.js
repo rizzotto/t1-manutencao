446f53e094ddf8c76503bb7fd74793dc932354d2
Pâmela <viingoflaas@gmail.com>
2019-09-09 18:27:17 -0300

    Retirada alterações de teste.

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -11,15 +11,7 @@ export default class QuestionContainer extends Component {
         
         return (
             <View style={{alignItems:"center"}}>
-                <DetailedRecordComponent
-                    titleText={"Peso: "} 
-                    descriptionText={"56 kg"}
-                    createdAt={"16/08/2019"}
-                    emailText={"cassandra.gomes@gmail.com"}
-                    birthDate={"20/08/1990"}
-                    weightText={"65 kg"}
-                    heightText={"170 cm"}
-                /> 
+                
             </View>
         )
     }
