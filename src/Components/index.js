b13db40d73306d931d61cd590597d3c0327bfadd
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2019-09-11 18:52:57 -0300

    16: Rafael
    Ajustar estilo do título e descrição

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,4 +2,4 @@
 export { default as CheckImage } from './CheckImage';
 export { default as ListItemSubitems } from './ListItemSubitems';
 export { default as Button } from './defaultButtonComponent';
-export { default as TitleDescription } from './titleDescComponent';
+export { default as TitleDescription } from './TitleDescComponent';
