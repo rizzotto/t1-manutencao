588047fa4e2fd81c70317595420c0ee62f768073
joaofleao <joaofleao@gmail.com>
2019-09-14 14:40:57 -0300

    24,25: Joao Leao e Guilherme Rizzotto finalizacao do container e da tela de ClosedList (remoção do componente de closed list

--- src/Containers/ClosedListContainer.js
+++ src/Containers/ClosedListContainer.js
@@ -1,129 +1,79 @@
 import React, {Component} from 'react';
-import { View, TextInput, StyleSheet, ScrollView } from "react-native";
-import ClosedListComponent from '../Components/ClosedListComponent';
+import { View, StyleSheet, ScrollView, FlatList } from "react-native";
 import TitleDescComponent from '../Components/TitleDescComponent';
-import ProgressBarComponent from '../Components/ProgressBarComponent';
 import DefaultButtonComponent from '../Components/defaultButtonComponent';
-
+import ItemListComponent from '../Components/ItemListComponent';
+import AppStyle from '../styles';
+/**
+     * @param dataToScreen Dados que serao rertornados para Screen (lista com itens selecionados)
+     * @param list Dados que estarao presentes na lista
+     * @param maxSelected Numero maximo de itens que podem ser selecionados na lista 
+     * @param titleText Titulo da pagina 
+     * @param descriptionText Descricao da pagina
+     * @param required Indicacao de se é necessario selecionar itens na lista 
+     * @return Container com titulo, descricao, lista fechada e botao de ir para proxima pagina
+     */
 
 export default class ClosedListContainer extends Component {
+    
+    state = {
+        list: this.props.list,
+        maxSelected: this.props.maxSelected,
+    };
+    
+    selectedItems = this.props.list.filter(x => x.isSelected)
 
-    list = [
-        {
-            id: '1',
-            isSelected: false,
-            texto: "teste1"
-        }, 
-        {
-            id: '2',
-            isSelected: true,
-            texto: "teste2"
-        },
-        {
-            id: '3',
-            isSelected: false,
-            texto: "teste3"
-            
-        },
-        {
-            id: '4',
-            isSelected: false,
-            texto: "teste4"
-        }, 
-        {
-            id: '5',
-            isSelected: true,
-            texto: "teste5"
-        },
-        {
-            id: '6',
-            isSelected: false,
-            texto: "teste6"
-            
-        },
-        {
-            id: '4',
-            isSelected: false,
-            texto: "teste4"
-        }, 
-        {
-            id: '5',
-            isSelected: true,
-            texto: "teste5"
-        },
-        {
-            id: '6',
-            isSelected: false,
-            texto: "teste6"
-            
-        },
-        {
-            id: '4',
-            isSelected: false,
-            texto: "teste4"
-        }, 
-        {
-            id: '5',
-            isSelected: true,
-            texto: "teste5"
-        },
-        {
-            id: '190',
-            isSelected: false,
-            texto: "190"
-            
-        }, 
-        {
-            id: '5',
-            isSelected: true,
-            texto: "teste5"
-        },
-        {
-            id: '6',
-            isSelected: false,
-            texto: "teste6"
-            
-        },
-        {
-            id: '4',
-            isSelected: false,
-            texto: "teste4"
-        }, 
-        {
-            id: '5',
-            isSelected: true,
-            texto: "teste5"
-        },
-        {
-            id: '190',
-            isSelected: false,
-            texto: "190"
-            
-        }
-    ];
-
-
-    render(){
+    _onPressItem = (index) => {
+        this.setState({ render: !this.render })
         
+        let numbSelected = this.state.list.filter(x => x.isSelected).length;
+        
+        if (numbSelected < this.state.maxSelected || this.state.list[index].isSelected) {
+            this.state.list[index].isSelected = !this.state.list[index].isSelected;
+            this.setState({ refresh: !this.state.refresh })
+        }
+        this.selectedItems = this.state.list.filter(x => x.isSelected)
+    };
+
+    dataToScreen = () => {
+        this.props.dataToScreen(this.selectedItems);
+    }
+    
+    render(){
+        selectedItems = this.selectedItems;
+        console.warn(this.minIsStaisfied)
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <ProgressBarComponent width={0.5}/>
                     <TitleDescComponent 
-                        titleText={"Medicamentos"} 
-                        descriptionText={"Informe os medicamentos que que você usa atualmente"}
+                        titleText={this.props.titleText} 
+                        descriptionText={this.props.descriptionText}
                     />
                 </View> 
                 <ScrollView style={styles.content}>
-                    <ClosedListComponent maxSelected={2} listItems={this.list}/>
+                    <FlatList
+                        data={this.state.list}
+                        renderItem={({ item, index }) => (
+                            <ItemListComponent 
+                                text={item.texto}
+                                onPress ={() => this._onPressItem(index)} 
+                                selected={item.isSelected}
+                            />
+                        )}
+                        keyExtractor={item => item.id.toString()}
+                        extraData={this.state.refresh}
+                        ItemSeparatorComponent={() => <Separator />}
+                    />
                 </ScrollView>
+                
                 <View style={styles.bottom}>
                     <DefaultButtonComponent 
-                    text={"Próximo"} 
-                    action={this.action}
-                    disabled={true}
-                    style={styles.buttonStyle} 
-                    textStyle={styles.textStyle}
+                        text={selectedItems.length==0? "Pular": "Próximo"}
+                        action={this.dataToScreen}
+                        style={styles.buttonStyle} 
+                        textStyle={styles.textStyle}
+                        extraData={this.state.refresh}
+                        onPress ={() => this._onPressItem(index)}
                     />     
                 </View> 
             </View>
@@ -131,13 +81,11 @@ export default class ClosedListContainer extends Component {
     }
 }
 
-
-
+const Separator = () => <View style={styles.line} />
 
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        // backgroundColor: '#3d3d',
     },
     header: {
     },
@@ -149,5 +97,9 @@ const styles = StyleSheet.create({
         marginBottom:'10%',
         alignContent:'flex-end'
     },
-
+    line: {
+        width: "100%",
+        height: 1,
+        backgroundColor: AppStyle.colors.lightGray
+    }
 })
---
+++ src/Screens/ClosedListScreen.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet } from "react-native";
+import ClosedListContainer from '../Containers/ClosedListContainer'
+import ProgressBarComponent from '../Components/ProgressBarComponent';
+    /**
+     * @param width Estado da barra de progresso
+     * @param list Dados que estarao presentes na lista
+     * @param maxSelected Numero maximo de itens que podem ser selecionados na lista 
+     * @param titleText Titulo da pagina 
+     * @param descriptionText Descricao da pagina
+     * 
+     * @return Tela com barra de progresso, titulo, descricao, lista fechada e botao de ir para proxima pagina
+     */
+export default class ClosedListScreen extends Component {
+    static navigationOptions = createDefaultNavigationOptions;
+
+    dataFromContainer = (data) => {
+        console.warn(data);
+    }
+
+    selectedItems = {}
+    render(){
+        const width = this.props.width || this.props.navigation.getParam("width");
+        const list = this.props.list || this.props.navigation.getParam("list");
+        const maxSelected = this.props.maxSelected || this.props.navigation.getParam("maxSelected");
+        const titleText = this.props.titleText || this.props.navigation.getParam("titleText");
+        const descriptionText = this.props.descriptionText || this.props.navigation.getParam("descriptionText");
+        return(
+            <SafeAreaView style={styles.container}>
+                <ProgressBarComponent width={width}/> 
+                <ClosedListContainer 
+                dataToScreen={this.dataFromContainer} 
+                list={list} 
+                maxSelected={maxSelected} 
+                titleText={titleText}
+                descriptionText={descriptionText}
+                />
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    }
+})
