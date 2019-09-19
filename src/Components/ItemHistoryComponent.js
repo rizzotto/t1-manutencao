315f5239bb586267506bba850ea8c35a925cebcb
joaofleao <joaofleao@gmail.com>
2019-09-19 17:20:00 -0300

    32: Joao Leao, Guilherme Rizzotto Estruturacao do componente

--- src/Components/ItemHistoryComponent.js
+++ src/Components/ItemHistoryComponent.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, TextInput } from "react-native";
+import {StyleSheet, View, TextInput, TouchableOpacity, Text } from "react-native";
 import AppStyle from '../styles';
     
 export default class ItemHistoryComponent extends Component {
-    
-}
+        render(){
+            return (
+                <View >
+                    <TouchableOpacity style={styles.button}>
+                        <Text >
+                                26
+                        </Text>
+                        <View style={styles.texts}>
+                            <Text >
+                                Pressão: Alta
+                            </Text>
+                            <Text >
+                                Sintomas: Dor de Cabeça
+                            </Text>
+                            <Text >
+                                Medicamentos: Dorflex
+                            </Text>
+                        </View>
+                        <Text >
+                                :)
+                        </Text>
+                        <Text >
+                                >
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+    }
 
 
 
-const createStyles = (selected) => {
-    return StyleSheet.create({
+const styles = StyleSheet.create({
+    button: {
+        flexDirection: "row"
+    },
+    texts: {
+        flex: 1
+    },
         
-    })
-}
+})
 
