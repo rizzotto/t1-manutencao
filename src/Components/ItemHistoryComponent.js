f81bce6a1f79d212d30a63b29e84318da8decb97
joaofleao <joaofleao@gmail.com>
2019-09-19 16:40:09 -0300

    32: Joao Leao, Guilherme Rizzotto Criacao do componente e da branch

---
+++ src/Components/ItemHistoryComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, TextInput } from "react-native";
+import AppStyle from '../styles';
+    
+export default class ItemHistoryComponent extends Component {
+    
+}
+
+
+
+const createStyles = (selected) => {
+    return StyleSheet.create({
+        
+    })
+}
+
