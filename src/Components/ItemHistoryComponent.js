7109329b974c51ef3c4daf665dbbebc92955c37b
joaofleao <joaofleao@gmail.com>
2019-09-23 19:17:01 -0300

    32: Guilherme Rizzotto, Joao Leao FInalizacao e documentacao do componente de historico

--- src/Components/ItemHistoryComponent.js
+++ src/Components/ItemHistoryComponent.js
@@ -1,32 +1,62 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, TextInput, TouchableOpacity, Text } from "react-native";
-import AppStyle from '../styles';
-    
+import {StyleSheet, View, TextInput, TouchableOpacity, Text, FlatList } from "react-native";
+import Arrow from './Arrow';
+/**
+     * @param hasEmoji Indica se o item possui emoji de representacao de sentimento
+     * @param list Lista de dados que estarao representados no item
+     * @param emoji Codigo do emoji a ser inserido na lista
+     * @return Component do item de historico
+     * EXAMPLE:
+     * <ItemHistoryComponent
+            list={DATA}
+            hasEmoji={true}
+            emoji={"😄"}
+        />
+
+        EMOJIS:
+        Raiva: 😡
+        Cansado: 😞
+        Chateado: 😕
+        Tanto Faz: 😐
+        Contente: 🙂
+        Feliz: 😁
+     */
+
 export default class ItemHistoryComponent extends Component {
         render(){
             return (
-                <View >
+
+                <View style={styles.item}>
                     <TouchableOpacity style={styles.button}>
-                        <Text >
+                        <Text style={styles.number}>
                                 26
                         </Text>
-                        <View style={styles.texts}>
-                            <Text >
-                                Pressão: Alta
-                            </Text>
-                            <Text >
-                                Sintomas: Dor de Cabeça
-                            </Text>
-                            <Text >
-                                Medicamentos: Dorflex
-                            </Text>
+                        <View style={this.props.hasEmoji ? styles.textsWithEmoji : styles.textsWithoutEmoji}>
+                            
+                            <FlatList
+                                data={this.props.list}
+                                renderItem={({ item }) => (
+                                    <Text style={styles.item}>
+                                        <Text style={styles.title}>
+                                            {item.id}
+                                        </Text>
+                                        <Text style={styles.text}>
+                                            {item.title}
+                                        </Text>
+                                    </Text>
+                                    )}
+                                keyExtractor={item => item.id}
+                            />
+         
                         </View>
-                        <Text >
-                                :)
-                        </Text>
-                        <Text >
-                                >
-                        </Text>
+                        {this.props.hasEmoji && 
+                        <Text style={styles.emoji}>
+                            {this.props.emoji}
+                        </Text>}
+                        
+                        <View style={styles.enter}>
+                            <Arrow/>
+                        </View>
                     </TouchableOpacity>
                 </View>
             )
@@ -36,12 +66,50 @@ export default class ItemHistoryComponent extends Component {
 
 
 const styles = StyleSheet.create({
+
     button: {
-        flexDirection: "row"
-    },
-    texts: {
-        flex: 1
-    },
+        flexDirection: "row",
+        alignItems: "center",
+        marginHorizontal: "3%"
         
+    },
+    number: {
+        width: "10%",
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+
+    },
+    textsWithEmoji: {
+        width: "65%",
+        padding: 15,
+    },
+    textsWithoutEmoji: {
+        width: "80%",
+        padding: 15
+    },
+    emoji: {
+        fontSize: 40,
+        width: "15%",
+        paddingLeft: 10,
+
+    },
+    enter: {
+        justifyContent: "center",
+        alignItems:"center",
+        width: "10%"
+    
+    }, 
+    item: {
+        flexDirection: "row",
+        textAlign: "justify"
+    },
+    title: {
+        fontWeight: "bold"
+    },
+    text: {
+        flex: 1
+    }
 })
 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
