6a0f950e5b4e0504e52feec7a8e65c2e9bde6e29
LucasJusto <lucas.dimer.justo@hotmail.com>
2019-09-26 19:27:57 -0300

    36: Lucas,Max adicionando props e estrutura

--- src/Containers/EmptyStateContainer.js
+++ src/Containers/EmptyStateContainer.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //import EmptyStateImage from '../Components/EmptyStateImage.png';
-
-
-import DefaultButtonComponent from '../Components/DefaultButtonComponent';
-import TitleDescComponent from '../Components/TitleDescComponent';
+import Button from '../Components';
+import TitleDescription from '../Components';
 import AppStyle from '../styles';
 
-export default class TextInputContainer extends Component {
+export default class EmptyStateContainer extends Component {
     render() {
+        buttonAction=this.props.buttonAction
+        if(this.props.local==='diario'){
+            title='Acompanhe seu dia-a-dia'
+            description='Com o HiperBem, você consegue acompanhar seu humor, medicação e sintomas diariamente.'
+            buttonTitle='Adicionar ao diário'
+        }
+        else if(this.props.local==='exames'){
+            title='Você no controle dos seus exames'
+            description='Com o HiperBem, você consegue centralizar os resultados dos seus exames médicos, poupando tempo e dinheiro!'
+            buttonTitle='Adicionar exames'
+        }
+        else if(this.props.local==='fichas'){
+            title='Seus dados clínicos sempre com você'
+            description='Com o HiperBem, você consegue centralizar suas informações clínicas e poupar tempo em consultas médicas.'
+            buttonTitle='Adicionar ficha'
+        }
         return (
-                <DefaultButtonComponent />
+            <View>
+                <TitleDescription titleText={title} descriptionText={description}/>
+                <Button text={buttonTitle} action={buttonAction}/>
+            </View>
         );
     }
 }
