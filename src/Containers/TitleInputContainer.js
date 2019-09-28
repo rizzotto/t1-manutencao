a60e986418e6a9e385b5dfe0c8176ef4e6813225
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2019-09-27 21:12:01 -0300

    D4: Rafael
    Ajustar tela de entrada de texto quando teclado está ativo.

--- src/Containers/TitleInputContainer.js
+++ src/Containers/TitleInputContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { TitleDescription, Button } from '../Components';
 import TextInputContainer from './TextInputContainer';
 
@@ -92,9 +93,9 @@ export default class TitleInputContainer extends Component{
     render(){
 
         return (
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <View style={styles.container}>
-                    <View style={styles.content}>
+            <View style={styles.container}>
+                <KeyboardAwareScrollView style={styles.content}>
+                    <View>
                         <TitleDescription
                             titleText={this.props.title}
                             descriptionText={this.props.description}
@@ -109,13 +110,13 @@ export default class TitleInputContainer extends Component{
                             type={this.props.keyboardType}
                         />
                     </View>
-                    <Button
-                        isDisabled={this.state.disabledButton}
-                        text={this.state.btnText}
-                        action={this.props.btnAction || this.dataToScreen}
-                    />
-                </View>
-            </TouchableWithoutFeedback>
+                </KeyboardAwareScrollView>
+                <Button
+                    isDisabled={this.state.disabledButton}
+                    text={this.state.btnText}
+                    action={this.props.btnAction || this.dataToScreen}
+                />
+            </View>
         );
     }
 }
