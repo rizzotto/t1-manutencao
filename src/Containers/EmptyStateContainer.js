0491a6f94ab5926d3829d4c8e8f0e2337b845809
Max <max.franke@acad.pucrs.br>
2019-09-30 18:23:01 -0300

    36: Lucas, Max, Rafael Container do empty state, usado quando o usuário ainda não fez nenhuma adição em algum fluxo.

--- src/Containers/EmptyStateContainer.js
+++ src/Containers/EmptyStateContainer.js
@@ -1,31 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Image, View } from 'react-native';
 import { Button, TitleDescription } from '../Components';
 import AppStyle from '../styles';
+/**
+ * @param local nome da tela em que o container será colocado. Pode ser: 'exames', 'diario' ou 'fichas'
+ * @param buttonAction ação do botão
+ *  - Example:
+ *    <EmptyStateContainer 
+ *          local={'fichas'} 
+ *          buttonAction={this._newRecord}
+ *    />
+ */
 
 export default class EmptyStateContainer extends Component {
     render() {
-        buttonAction=this.props.buttonAction
-        if(this.props.local==='diario'){
-            title='Acompanhe seu dia-a-dia'
-            description='Com o HiperBem, você consegue acompanhar seu humor, medicação e sintomas diariamente.'
-            buttonTitle='Adicionar ao diário'
+        buttonAction = this.props.buttonAction
+        if (this.props.local === 'diario') {
+            title = 'Acompanhe seu dia-a-dia'
+            description = 'Com o HiperBem, você consegue acompanhar seu humor, medicação e sintomas diariamente.'
+            buttonTitle = 'Adicionar ao diário'
         }
-        else if(this.props.local==='exames'){
-            title='Você no controle dos seus exames'
-            description='Com o HiperBem, você consegue centralizar os resultados dos seus exames médicos, poupando tempo e dinheiro!'
-            buttonTitle='Adicionar exames'
+        else if (this.props.local === 'exames') {
+            title = 'Você no controle dos seus exames'
+            description = 'Com o HiperBem, você consegue centralizar os resultados dos seus exames médicos, poupando tempo e dinheiro!'
+            buttonTitle = 'Adicionar exames'
         }
-        else if(this.props.local==='fichas'){
-            title='Seus dados clínicos sempre com você'
-            description='Com o HiperBem, você consegue centralizar suas informações clínicas e poupar tempo em consultas médicas.'
-            buttonTitle='Adicionar ficha'
+        else if (this.props.local === 'fichas') {
+            title = 'Seus dados clínicos sempre com você'
+            description = 'Com o HiperBem, você consegue centralizar suas informações clínicas e poupar tempo em consultas médicas.'
+            buttonTitle = 'Adicionar ficha'
         }
         return (
-            <View>
-                <Image source={require('../Resources/emptyState.png')}/>
-                <TitleDescription titleText={title} descriptionText={description}/>
-                <Button text={buttonTitle} action={buttonAction}/>
+            <View style={styles.container}>
+
+                <Image 
+                style={{width: '30%', height: '25%', alignSelf:'center'}} 
+                source={require('../Resources/emptyState.png')} />
+                <TitleDescription 
+                styleView={styles.titleDescView}
+                styleTitle={styles.title}
+                styleDescription={styles.description}
+                titleText={title} descriptionText={description} />
+                <Button text={buttonTitle} action={buttonAction} />
             </View>
         );
     }
@@ -33,13 +49,28 @@ export default class EmptyStateContainer extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 20,
+        alignItems: "stretch",
+        justifyContent:'center',
         backgroundColor: AppStyle.colors.background,
     },
-    enter: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: "10%"
-
+    titleDescView: {
+        marginTop: 25,
+        marginBottom: 10,
+        textAlign:'center'
     },
+    text:{
+        textAlign:'center',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    title:{
+        marginTop:0,
+        marginBottom:10,
+        textAlign:'center',
+    },
+    description:{
+        textAlign:'center',
+    }
+
+
 });
