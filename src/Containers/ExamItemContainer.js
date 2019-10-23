a07a5ddd8850cc0ef876a3c020438e7dd96c8420
joaofleao <joaofleao@gmail.com>
2019-10-23 18:10:35 -0300

    42: Gabriel Franzoni, Joao Leao
    initial commit

---
+++ src/Containers/ExamItemContainer.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import AppStyle from '../styles';
+/**
+     * @param 
+     * @return Container do item da lista de exames
+     */
+
+export default class ExamItemContainer extends Component {
+
+    render(){
+        return(
+            <View style={styles.container} >
+                
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+})
