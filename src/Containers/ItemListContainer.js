1586bc06e004122d2b26aa91679a25bb9c3d5741
LucasJusto <lucas.dimer.justo@hotmail.com>
2019-10-26 16:59:34 -0300

    38: Lucas Justo, Guilherme Rizzotto, Rafael Araujo
    Limpando o código e adicionando javadoc ao container ja concluido.

--- src/Containers/ItemListContainer.js
+++ src/Containers/ItemListContainer.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity, Image} from 'react-native';
-import AppStyle from '../styles';
+import {StyleSheet, View, FlatList, Image} from 'react-native';
 import ImageComponent from '../Components/ImageComponent';
 
 
@@ -8,11 +7,20 @@ import ImageComponent from '../Components/ImageComponent';
 /**
  * @author Guilherme Rizzotto, Lucas Justo
  * 
- * @param
- * Uso do StyleSheet: StyleSheet.create({ ... });
+ * @param add boolean que diz se haverá a opção de adicionar fotos (+)
+ * @param data lista de imagens a serem mostradas pelo container
+ * @param addAction método para o botão add
+ * @param onSelectItem método que trata o click nas fotos (recebe indice por parametro)
+ * @param isTouchable boolean que diz se as imagens da lista serão clickaveis
  * 
- * Exemplo de uso: <ExamItemContainer></ExamItemContainer>
- * @return Container do Item dos Exames
+ * @return Container da lista de imagem
+ * Exemplo de uso: 
+ * <ItemListContainer 
+ *  add={true} 
+ *  data={imageList} 
+ *  addAction={selectImages}
+ *  onSelectItem={onClickImage}
+ *  isTouchable={true} />
  */
 
 export default class ItemListContainer extends Component {
@@ -21,23 +29,19 @@ export default class ItemListContainer extends Component {
         add = this.props.add
         this.format()
     }
-    defaultAction = () => {
-        console.log("Funcionalidade não implementada");
-    }
     state = {
-        // O data precisa ser futuramente substituido por callbacks
         data: this.props.data
     };
 
     
     format = () =>{
-        // console.warn(this.state.data)
         if(add){
             this.state.data.unshift({id:0,sourceImage:require("../Resources/add.png")});
             for(var i=1;i<this.state.data.length;i++){
                 this.state.data[i].id += 1
             }
         }
+        
         whiteImages = 3-(this.state.data.length%3)
         for(var i=1;i<=whiteImages;i++){
             this.state.data.push({id:this.state.data.length-1+i, sourceImage: require("../Resources/whiteImage.png"), white: true})
@@ -45,14 +49,14 @@ export default class ItemListContainer extends Component {
     }
 
     onClickItem = (index) => {
-        console.log(index)
         if (add) {
             if (index === 0) {
                 this.props.addAction()
             } else {
                 this.props.onSelectItem(index - 1)
             }
-        } else {
+        } 
+        else {
             this.props.onSelectItem(index)
         }
     }
@@ -61,19 +65,19 @@ export default class ItemListContainer extends Component {
         return(
             <View style = {styles.container}>
                 <FlatList
-                        style={styles.list}
-                        numColumns={3}
-                        initialNumToRender={5}
-                        data={this.state.data}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item, index }) => {
-                            return (
-                                <View style={styles.item}>
-                                    {item.white?<Image source={item.sourceImage} style={styles.image}></Image>
-                                    :<ImageComponent onClick={() => this.onClickItem(index)} isTouch={this.props.isTouchable} style = {styles.image} {...item}/>}
-                                </View>
-                            );   
-                        }}
+                    style={styles.list}
+                    numColumns={3}
+                    initialNumToRender={5}
+                    data={this.state.data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <View style={styles.item}>
+                                {item.white?<Image source={item.sourceImage} style={styles.image}></Image>
+                                :<ImageComponent onClick={() => this.onClickItem(index)} isTouch={this.props.isTouchable} style = {styles.image} {...item}/>}
+                            </View>
+                        );   
+                    }}
                 />
             </View>
         )
