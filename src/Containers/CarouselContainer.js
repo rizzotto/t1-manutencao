5eadecf266da83444418b5ce039f14830cf6ebfa
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2019-10-26 16:13:39 -0300

    Estilo emoji: Rafael
    Ajustar estilo dos emojis na tela do diário.

--- src/Containers/CarouselContainer.js
+++ src/Containers/CarouselContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, Item, StyleSheet, SafeAreaView } from 'react-native';
+import { FlatList, View, StyleSheet, Dimensions } from 'react-native';
 import CardEmojiComponent from '../Components/CardEmojiComponent'; 
-import Arrow from '../Components/Arrow';
+import { Arrow } from '../Components';
 
 /**
  * @author Bruno Guerra e Eduardo Lessa
@@ -20,33 +20,67 @@ const DATA = [
 ];
 
 export default class CarouselContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        const itemsPerPage = 3
+        const data = props.data || DATA
+
+        this.state = {
+            page: 0,
+            data,
+            maxPages: Math.ceil(data.length / itemsPerPage),
+            itemsPerPage
+        }
+    }
 
     handlePress(item){
         this.props.action(item);
     }
 
+    showNextPage = () => {
+        const { page: currentPage, itemsPerPage } = this.state
+        const nextPageIndex = (currentPage + 1) * itemsPerPage
+
+        this.listRef.scrollToIndex({ index: nextPageIndex, animated: true, viewPosition: 0 })
+        this.setState({ ...this.state, page: currentPage + 1 })
+    }
+
+    showPreviousPage = () => {
+        const { page: currentPage, itemsPerPage } = this.state
+        const previousPageIndex = (currentPage - 1) * itemsPerPage
+
+        this.listRef.scrollToIndex({ index: previousPageIndex, animated: true, viewPosition: 0 })
+        this.setState({ ...this.state, page: currentPage - 1 })
+    }
+
     render() {
+        const { page, maxPages } = this.state
+        const isFirstPage = page === 0
+        const isLastPage = page === maxPages - 1
 
         return (
-
-            <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+                <Arrow direction="left" onPress={this.showPreviousPage} disabled={isFirstPage} />
                 <FlatList
+                    ref={(list) => this.listRef = list}
                     data={this.props.data || DATA}
                     renderItem={({ item }) => (
-                    <View>
-                        <CardEmojiComponent
-                        text={item.text}
-                        emoji={item.emoji}
-                        onPress={() => this.handlePress(item)}
+                        <CardEmojiComponent style={styles.item}
+                            text={item.text}
+                            emoji={item.emoji}
+                            onPress={() => this.handlePress(item)}
                         />
-                    </View>
                     )}
-                    keyExtractor={item => item.text }
+                    keyExtractor={item => item.text}
+                    scrollEnabled={false}
                     horizontal={true}
+                    pagingEnabled={true}
+                    decelerationRate="fast"
                     showsHorizontalScrollIndicator={false}
                 />
-                <Arrow></Arrow>
-            </SafeAreaView>
+                <Arrow direction="right" onPress={this.showNextPage} disabled={isLastPage} />
+            </View>
         )
     }
 
@@ -56,7 +90,22 @@ export default class CarouselContainer extends Component {
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        alignItems: 'center',
-        marginLeft: 15,
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginHorizontal: 10
+    },
+    item: {
+        marginHorizontal: 4,
+        marginVertical: 10,
+        minHeight: 130,
+
+        // a FlatList não é horizontal, então a largura dos itens não vem dela
+        // (como foi feito no CardEmojiContainer), mas dos itens em si;
+        // ou é definir a largura "na mão", ou se aventurar com FlatLists
+        // muito aninhadas e com direções de scroll diferentes
+        // (o que não temos tempo para fazer no momento);
+        // ainda não fica perfeito em telas de qualquer tamanho,
+        // mas é o que dá para fazer com o tempo que temos
+        width: Dimensions.get("window").width * 0.25
     }
 });
