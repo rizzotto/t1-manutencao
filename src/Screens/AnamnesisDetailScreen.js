a99b8c298b8afa5b4d225b391cf4bcfa91626b0e
joaofleao <joaofleao@gmail.com>
2019-11-07 18:16:16 -0200

    50: Joao Leao, Eduardo Lessa Adição do botão de exportar

--- src/Screens/AnamnesisDetailScreen.js
+++ src/Screens/AnamnesisDetailScreen.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import RecordDetailContainer from '../Containers/RecordDetailContainer';
+import { HeaderTextButtonComponent } from '../Components';
 
 export default class AnamnesisDetailScreen extends Component{
-
+    static navigationOptions = ({ navigation }) => {
+        return {
+            headerRight: (
+                <HeaderTextButtonComponent
+                    text='Exportar'
+                    hasColor="true"
+                    onPress={() => {console.warn("Funcionalidade não Implementada")}}
+                />
+            )
+        }
+    }
     render(){
         return(
             <SafeAreaView>
