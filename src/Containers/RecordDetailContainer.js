3b148d457d5066e01ba9c226f83a2ed1c2a415ed
Eduardo Lessa <eduardo.lessa@acad.pucrs.br>
2019-11-08 18:26:40 -0200

    50: Adicionada dependencia para gerar PDF Ainda falta abrir a opçao de share para o usuario escolher para onde enviar o PDF E pegar o conteudo da ficha para gerar o PDF

--- src/Containers/RecordDetailContainer.js
+++ src/Containers/RecordDetailContainer.js
@@ -2,81 +2,109 @@ import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView, Text } from 'react-native';
 import TitleDescComponent from '../Components/TitleDescComponent';
 import DetailedRecordComponent from '../Components/DetailedRecordComponent';
-import {formatDate, formatHeight, formatArrayWithSeparator, formatArrayObjectsAnam, textWhenEmpty} from '../Utils/Helpers';
+import { formatDate, formatHeight, formatArrayWithSeparator, formatArrayObjectsAnam, textWhenEmpty } from '../Utils/Helpers';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export default class RecordDetailContainer extends Component {
+
+
+  componentDidMount(){
+    this.createPDF();
+  }
+
+
+  async createPDF() {
+    let options = {
+      html: '<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>',
+      fileName: 'test',
+      base64: true
+    };
+
+    const file = await RNHTMLtoPDF.convert(options)
+    let filePath = RNFetchBlob.fs.dirs.DownloadDir + '/testPDF.pdf';
+
+    RNFetchBlob.fs.writeFile(filePath, file.base64, 'base64')
+
+    alert(filePath);
+  }
+
+
+
+
+
   render() {
     return (
       <ScrollView style={styles.container}>
         <TitleDescComponent
-        styleView={styles.styleView}
-        styleTitle={styles.generalTitle}
-        styleDescription={styles.generalDescription}
-        titleText={this.props.anamnese.name}
-        descriptionText={formatDate(this.props.anamnese.creationDate)} />
+          styleView={styles.styleView}
+          styleTitle={styles.generalTitle}
+          styleDescription={styles.generalDescription}
+          titleText={this.props.anamnese.name}
+          descriptionText={formatDate(this.props.anamnese.creationDate)} />
 
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"E-mail:"}
-        descriptionText={textWhenEmpty(this.props.anamnese.email, 'N/A')}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"E-mail:"}
+          descriptionText={textWhenEmpty(this.props.anamnese.email, 'N/A')} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Data de nascimento:"}
-        descriptionText={formatDate(this.props.anamnese.birthDate)}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Data de nascimento:"}
+          descriptionText={formatDate(this.props.anamnese.birthDate)} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Peso e altura:"}
-        descriptionText={this.props.anamnese.weight+" kg, "+formatHeight(this.props.anamnese.height)}/>
-        <View style={styles.divisionBar}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Peso e altura:"}
+          descriptionText={this.props.anamnese.weight + " kg, " + formatHeight(this.props.anamnese.height)} />
+        <View style={styles.divisionBar} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Principais Sintomas:"}
-        descriptionText={formatArrayWithSeparator(this.props.anamnese.symptoms, ', ')}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Principais Sintomas:"}
+          descriptionText={formatArrayWithSeparator(this.props.anamnese.symptoms, ', ')} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Medicamentos:"}
-        descriptionText={formatArrayObjectsAnam(this.props.anamnese.medicines)}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Medicamentos:"}
+          descriptionText={formatArrayObjectsAnam(this.props.anamnese.medicines)} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Patologias:"}
-        descriptionText={formatArrayWithSeparator(this.props.anamnese.pathologies, ', ')}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Patologias:"}
+          descriptionText={formatArrayWithSeparator(this.props.anamnese.pathologies, ', ')} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Histórico Familiar:"}
-        descriptionText={formatArrayWithSeparator(this.props.anamnese.familyPathologies, ', ')}/>
-        <View style={styles.divisionBar}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Histórico Familiar:"}
+          descriptionText={formatArrayWithSeparator(this.props.anamnese.familyPathologies, ', ')} />
+        <View style={styles.divisionBar} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Hábitos:"}
-        descriptionText={formatArrayObjectsAnam(this.props.anamnese.habits)}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Hábitos:"}
+          descriptionText={formatArrayObjectsAnam(this.props.anamnese.habits)} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Estilo de vida:"}
-        descriptionText={textWhenEmpty(this.props.anamnese.lifeRhythm, 'N/A')}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Estilo de vida:"}
+          descriptionText={textWhenEmpty(this.props.anamnese.lifeRhythm, 'N/A')} />
         <DetailedRecordComponent
-        styleDescription={styles.detailRecordDescription}
-        styleTitle={styles.detailRecordTitle}
-        styleView={styles.styleView}
-        titleText={"Alimentação:"}
-        descriptionText={textWhenEmpty(this.props.anamnese.eatingStyle, 'N/A')}/>
+          styleDescription={styles.detailRecordDescription}
+          styleTitle={styles.detailRecordTitle}
+          styleView={styles.styleView}
+          titleText={"Alimentação:"}
+          descriptionText={textWhenEmpty(this.props.anamnese.eatingStyle, 'N/A')} />
       </ScrollView>
     );
   }
@@ -87,29 +115,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   divisionBar: {
-      borderBottomColor: 'gray',
-      borderBottomWidth: 1,
-      marginTop: 12,
-      marginBottom: 7
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    marginTop: 12,
+    marginBottom: 7
   },
-  styleView:{
+  styleView: {
     paddingVertical: 5,
     paddingRight: 7,
     paddingLeft: 7
   },
-  detailRecordTitle:{
+  detailRecordTitle: {
     paddingVertical: 0,
-    fontWeight:'bold',
+    fontWeight: 'bold',
   },
-  detailRecordDescription:{
+  detailRecordDescription: {
     paddingHorizontal: 0
   },
-  generalTitle:{
-    fontWeight:"bold",
+  generalTitle: {
+    fontWeight: "bold",
     marginLeft: 7,
     marginTop: 15,
   },
-  generalDescription:{
+  generalDescription: {
     marginLeft: 8,
     marginTop: 10,
     marginBottom: 4,
